e1a4b0f3c0e7b3dc250da510408f182392a0ca6c
Jacek K <szachy960@gmail.com>
2020-08-27 16:43:06 +0200

    positions 1-100

---
+++ README.md
@@ -0,0 +1,52 @@
+# Fischer chess position generator
+> Chess 960 - position generator
+
+## Table of contents
+* [General info](#general-info)
+* [Screenshots](#screenshots)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Features](#features)
+* [Status](#status)
+* [Credits](#credits)
+* [Contact](#contact)
+
+## General info
+Position generator - Fisher chess (Chess 960)
+
+## Screenshots
+Home Page
+![Home page](./img/home.jpg)
+All piositions
+![All positions page](./img/allpositions.jpg)
+Random position Page
+![Random position page](./img/random.jpg)
+Position Page
+![Position page](./img/positon.jpg)
+
+
+## Technologies
+* Flask 1.1.2
+* Python 3.7
+* Bootstrap 
+
+## Setup
+Use pip installation to install dependencies from requirements.txt
+## Code requirements.txt
+
+`$ pip install -r requirements.txt`
+
+## Features
+To-do list:
+* Add positions 101-960
+
+
+## Status
+Project is: _in progress_, Positions 101-960
+
+## Credits
+
+
+## Contact
+Created by [Jacek960](mailto:j.kuciel@outlook.com)- feel free to contact me!
+
--- positions.py
+++ positions.py
@@ -1,74 +1,602 @@
 allpositions = [
-        {
-            'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-            'img':'1.png',
-            'position':0,
-            'name':'Position no 1'
-        },
-        {
-            'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-            'img':'2.png',
-            'position':1,
-            'name':'Position no 2'
-        },
     {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-        'img': '1.png',
+        'fen': 'bqnbnrkr/pppppppp/8/8/8/8/PPPPPPPP/BQNBNRKR',
+        'img': '1.jpg',
+        'position': 0,
+        'name': 'Position no 1'
+    },
+    {
+        'fen': 'bqnnrbkr/pppppppp/8/8/8/8/PPPPPPPP/BQNNRBKR',
+        'img': '2.jpg',
+        'position': 1,
+        'name': 'Position no 2'
+    },
+    {
+        'fen': 'bqnnrkrb/pppppppp/8/8/8/8/PPPPPPPP/BQNNRKRB',
+        'img': '3.jpg',
         'position': 2,
         'name': 'Position no 3'
     },
     {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-        'img': '1.png',
+        'fen': 'qbbnnrkr/pppppppp/8/8/8/8/PPPPPPPP/QBBNNRKR',
+        'img': '4.jpg',
         'position': 3,
         'name': 'Position no 4'
     },
     {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-        'img': '2.png',
+        'fen': 'qnbbnrkr/pppppppp/8/8/8/8/PPPPPPPP/QNBBNRKR',
+        'img': '5.jpg',
         'position': 4,
         'name': 'Position no 5'
     },
     {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-        'img': '1.png',
+        'fen': 'qnbnrbkr/pppppppp/8/8/8/8/PPPPPPPP/QNBNRBKR',
+        'img': '6.jpg',
         'position': 5,
         'name': 'Position no 6'
     },
     {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-        'img': '2.png',
+        'fen': 'qnbnrkrb/pppppppp/8/8/8/8/PPPPPPPP/QNBNRKRB',
+        'img': '7.jpg',
         'position': 6,
         'name': 'Position no 7'
     },
     {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-        'img': '1.png',
+        'fen': 'qbnnbrkr/pppppppp/8/8/8/8/PPPPPPPP/QBNNBRKR',
+        'img': '8.jpg',
         'position': 7,
         'name': 'Position no 8'
     },
     {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-        'img': '1.png',
+        'fen': 'qnnbbrkr/pppppppp/8/8/8/8/PPPPPPPP/QNNBBRKR',
+        'img': '9.jpg',
         'position': 8,
         'name': 'Position no 9'
     },
     {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-        'img': '1.png',
+        'fen': 'qnnrbbkr/pppppppp/8/8/8/8/PPPPPPPP/QNNRBBKR',
+        'img': '10.jpg',
         'position': 9,
         'name': 'Position no 10'
     },
     {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-        'img': '1.png',
+        'fen': 'qnnrbkrb/pppppppp/8/8/8/8/PPPPPPPP/QNNRBKRB',
+        'img': '11.jpg',
         'position': 10,
         'name': 'Position no 11'
     },
     {
-        'fen': 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
-        'img': '1.png',
+        'fen': 'qbnnrkbr/pppppppp/8/8/8/8/PPPPPPPP/QBNNRKBR',
+        'img': '12.jpg',
         'position': 11,
         'name': 'Position no 12'
     },
-    ]
+    {
+        'fen': 'qnnbrkbr/pppppppp/8/8/8/8/PPPPPPPP/QNNBRKBR',
+        'img': '13.jpg',
+        'position': 12,
+        'name': 'Position no 13'
+    },
+    {
+        'fen': 'qnnrkbbr/pppppppp/8/8/8/8/PPPPPPPP/QNNRKBBR',
+        'img': '14.jpg',
+        'position': 13,
+        'name': 'Position no 14'
+    },
+    {
+        'fen': 'qnnrkrbb/pppppppp/8/8/8/8/PPPPPPPP/QNNRKRBB',
+        'img': '15.jpg',
+        'position': 14,
+        'name': 'Position no 15'
+    },
+    {
+        'fen': 'bbnqnrkr/pppppppp/8/8/8/8/PPPPPPPP/BBNQNRKR',
+        'img': '16.jpg',
+        'position': 15,
+        'name': 'Position no 16'
+    },
+    {
+        'fen': 'bnqbnrkr/pppppppp/8/8/8/8/PPPPPPPP/BNQBNRKR',
+        'img': '17.jpg',
+        'position': 16,
+        'name': 'Position no 17'
+    },
+    {
+        'fen': 'bnqnrbkr/pppppppp/8/8/8/8/PPPPPPPP/BNQNRBKR',
+        'img': '18.jpg',
+        'position': 17,
+        'name': 'Position no 18'
+    },
+    {
+        'fen': 'bnqnrkrb/pppppppp/8/8/8/8/PPPPPPPP/BNQNRKRB',
+        'img': '19.jpg',
+        'position': 18,
+        'name': 'Position no 19'
+    },
+    {
+        'fen': 'nbbqnrkr/pppppppp/8/8/8/8/PPPPPPPP/NBBQNRKR',
+        'img': '20.jpg',
+        'position': 19,
+        'name': 'Position no 20'
+    },
+    {
+        'fen': 'nqbbnrkr/pppppppp/8/8/8/8/PPPPPPPP/NQBBNRKR',
+        'img': '21.jpg',
+        'position': 20,
+        'name': 'Position no 21'
+    },
+    {
+        'fen': 'nqbnrbkr/pppppppp/8/8/8/8/PPPPPPPP/NQBNRBKR',
+        'img': '22.jpg',
+        'position': 21,
+        'name': 'Position no 22'
+    },
+    {
+        'fen': 'nqbnrkrb/pppppppp/8/8/8/8/PPPPPPPP/NQBNRKRB',
+        'img': '23.jpg',
+        'position': 22,
+        'name': 'Position no 23'
+    },
+    {
+        'fen': 'nbqnbrkr/pppppppp/8/8/8/8/PPPPPPPP/NBQNBRKR',
+        'img': '24.jpg',
+        'position': 23,
+        'name': 'Position no 24'
+    },
+    {
+        'fen': 'nqnbbrkr/pppppppp/8/8/8/8/PPPPPPPP/NQNBBRKR',
+        'img': '25.jpg',
+        'position': 24,
+        'name': 'Position no 25'
+    },
+    {
+        'fen': 'nqnrbbkr/pppppppp/8/8/8/8/PPPPPPPP/NQNRBBKR',
+        'img': '26.jpg',
+        'position': 25,
+        'name': 'Position no 26'
+    },
+    {
+        'fen': 'nqnrbkrb/pppppppp/8/8/8/8/PPPPPPPP/NQNRBKRB',
+        'img': '27.jpg',
+        'position': 26,
+        'name': 'Position no 27'
+    },
+    {
+        'fen': 'nbqnrkbr/pppppppp/8/8/8/8/PPPPPPPP/NBQNRKBR',
+        'img': '28.jpg',
+        'position': 27,
+        'name': 'Position no 28'
+    },
+    {
+        'fen': 'nqnbrkbr/pppppppp/8/8/8/8/PPPPPPPP/NQNBRKBR',
+        'img': '29.jpg',
+        'position': 28,
+        'name': 'Position no 29'
+    },
+    {
+        'fen': 'nqnrkbbr/pppppppp/8/8/8/8/PPPPPPPP/NQNRKBBR',
+        'img': '30.jpg',
+        'position': 29,
+        'name': 'Position no 30'
+    },
+    {
+        'fen': 'nqnrkrbb/pppppppp/8/8/8/8/PPPPPPPP/NQNRKRBB',
+        'img': '31.jpg',
+        'position': 30,
+        'name': 'Position no 31'
+    },
+    {
+        'fen': 'bbnnqrkr/pppppppp/8/8/8/8/PPPPPPPP/BBNNQRKR',
+        'img': '32.jpg',
+        'position': 31,
+        'name': 'Position no 32'
+    },
+    {
+        'fen': 'bnnbqrkr/pppppppp/8/8/8/8/PPPPPPPP/BNNBQRKR',
+        'img': '33.jpg',
+        'position': 32,
+        'name': 'Position no 33'
+    },
+    {
+        'fen': 'bnnqrbkr/pppppppp/8/8/8/8/PPPPPPPP/BNNQRBKR',
+        'img': '34.jpg',
+        'position': 33,
+        'name': 'Position no 34'
+    },
+    {
+        'fen': 'bnnqrkrb/pppppppp/8/8/8/8/PPPPPPPP/BNNQRKRB',
+        'img': '35.jpg',
+        'position': 34,
+        'name': 'Position no 35'
+    },
+    {
+        'fen': 'nbbnqrkr/pppppppp/8/8/8/8/PPPPPPPP/NBBNQRKR',
+        'img': '36.jpg',
+        'position': 35,
+        'name': 'Position no 36'
+    },
+    {
+        'fen': 'nnbbqrkr/pppppppp/8/8/8/8/PPPPPPPP/NNBBQRKR',
+        'img': '37.jpg',
+        'position': 36,
+        'name': 'Position no 37'
+    },
+    {
+        'fen': 'nnbqrbkr/pppppppp/8/8/8/8/PPPPPPPP/NNBQRBKR',
+        'img': '38.jpg',
+        'position': 37,
+        'name': 'Position no 38'
+    },
+    {
+        'fen': 'nnbqrkrb/pppppppp/8/8/8/8/PPPPPPPP/NNBQRKRB',
+        'img': '39.jpg',
+        'position': 38,
+        'name': 'Position no 39'
+    },
+    {
+        'fen': 'nbnqbrkr/pppppppp/8/8/8/8/PPPPPPPP/NBNQBRKR',
+        'img': '40.jpg',
+        'position': 39,
+        'name': 'Position no 40'
+    },
+    {
+        'fen': 'nnqbbrkr/pppppppp/8/8/8/8/PPPPPPPP/NNQBBRKR',
+        'img': '41.jpg',
+        'position': 40,
+        'name': 'Position no 41'
+    },
+    {
+        'fen': 'nnqrbbkr/pppppppp/8/8/8/8/PPPPPPPP/NNQRBBKR',
+        'img': '42.jpg',
+        'position': 41,
+        'name': 'Position no 42'
+    },
+    {
+        'fen': 'nnqrbkrb/pppppppp/8/8/8/8/PPPPPPPP/NNQRBKRB',
+        'img': '43.jpg',
+        'position': 42,
+        'name': 'Position no 43'
+    },
+    {
+        'fen': 'nbnqrkbr/pppppppp/8/8/8/8/PPPPPPPP/NBNQRKBR',
+        'img': '44.jpg',
+        'position': 43,
+        'name': 'Position no 44'
+    },
+    {
+        'fen': 'nnqbrkbr/pppppppp/8/8/8/8/PPPPPPPP/NNQBRKBR',
+        'img': '45.jpg',
+        'position': 44,
+        'name': 'Position no 45'
+    },
+    {
+        'fen': 'nnqrkbbr/pppppppp/8/8/8/8/PPPPPPPP/NNQRKBBR',
+        'img': '46.jpg',
+        'position': 45,
+        'name': 'Position no 46'
+    },
+    {
+        'fen': 'nnqrkrbb/pppppppp/8/8/8/8/PPPPPPPP/NNQRKRBB',
+        'img': '47.jpg',
+        'position': 46,
+        'name': 'Position no 47'
+    },
+    {
+        'fen': 'bbnnrqkr/pppppppp/8/8/8/8/PPPPPPPP/BBNNRQKR',
+        'img': '48.jpg',
+        'position': 47,
+        'name': 'Position no 48'
+    },
+    {
+        'fen': 'bnnbrqkr/pppppppp/8/8/8/8/PPPPPPPP/BNNBRQKR',
+        'img': '49.jpg',
+        'position': 48,
+        'name': 'Position no 49'
+    },
+    {
+        'fen': 'bnnrqbkr/pppppppp/8/8/8/8/PPPPPPPP/BNNRQBKR',
+        'img': '50.jpg',
+        'position': 49,
+        'name': 'Position no 50'
+    },
+    {
+        'fen': 'bnnrqkrb/pppppppp/8/8/8/8/PPPPPPPP/BNNRQKRB',
+        'img': '51.jpg',
+        'position': 50,
+        'name': 'Position no 51'
+    },
+    {
+        'fen': 'nbbnrqkr/pppppppp/8/8/8/8/PPPPPPPP/NBBNRQKR',
+        'img': '52.jpg',
+        'position': 51,
+        'name': 'Position no 52'
+    },
+    {
+        'fen': 'nnbbrqkr/pppppppp/8/8/8/8/PPPPPPPP/NNBBRQKR',
+        'img': '53.jpg',
+        'position': 52,
+        'name': 'Position no 53'
+    },
+    {
+        'fen': 'nnbrqbkr/pppppppp/8/8/8/8/PPPPPPPP/NNBRQBKR',
+        'img': '54.jpg',
+        'position': 53,
+        'name': 'Position no 54'
+    },
+    {
+        'fen': 'nnbrqkrb/pppppppp/8/8/8/8/PPPPPPPP/NNBRQKRB',
+        'img': '55.jpg',
+        'position': 54,
+        'name': 'Position no 55'
+    },
+    {
+        'fen': 'nbnrbqkr/pppppppp/8/8/8/8/PPPPPPPP/NBNRBQKR',
+        'img': '56.jpg',
+        'position': 55,
+        'name': 'Position no 56'
+    },
+    {
+        'fen': 'nnrbbqkr/pppppppp/8/8/8/8/PPPPPPPP/NNRBBQKR',
+        'img': '57.jpg',
+        'position': 56,
+        'name': 'Position no 57'
+    },
+    {
+        'fen': 'nnrqbbkr/pppppppp/8/8/8/8/PPPPPPPP/NNRQBBKR',
+        'img': '58.jpg',
+        'position': 57,
+        'name': 'Position no 58'
+    },
+    {
+        'fen': 'nnrqbkrb/pppppppp/8/8/8/8/PPPPPPPP/NNRQBKRB',
+        'img': '59.jpg',
+        'position': 58,
+        'name': 'Position no 59'
+    },
+    {
+        'fen': 'nbnrqkbr/pppppppp/8/8/8/8/PPPPPPPP/NBNRQKBR',
+        'img': '60.jpg',
+        'position': 59,
+        'name': 'Position no 60'
+    },
+    {
+        'fen': 'nnrbqkbr/pppppppp/8/8/8/8/PPPPPPPP/NNRBQKBR',
+        'img': '61.jpg',
+        'position': 60,
+        'name': 'Position no 61'
+    },
+    {
+        'fen': 'nnrqkbbr/pppppppp/8/8/8/8/PPPPPPPP/NNRQKBBR',
+        'img': '62.jpg',
+        'position': 61,
+        'name': 'Position no 62'
+    },
+    {
+        'fen': 'nnrqkrbb/pppppppp/8/8/8/8/PPPPPPPP/NNRQKRBB',
+        'img': '63.jpg',
+        'position': 62,
+        'name': 'Position no 63'
+    },
+    {
+        'fen': 'bbnnrkqr/pppppppp/8/8/8/8/PPPPPPPP/BBNNRKQR',
+        'img': '64.jpg',
+        'position': 63,
+        'name': 'Position no 64'
+    },
+    {
+        'fen': 'bnnbrkqr/pppppppp/8/8/8/8/PPPPPPPP/BNNBRKQR',
+        'img': '65.jpg',
+        'position': 64,
+        'name': 'Position no 65'
+    },
+    {
+        'fen': 'bnnrkbqr/pppppppp/8/8/8/8/PPPPPPPP/BNNRKBQR',
+        'img': '66.jpg',
+        'position': 65,
+        'name': 'Position no 66'
+    },
+    {
+        'fen': 'bnnrkqrb/pppppppp/8/8/8/8/PPPPPPPP/BNNRKQRB',
+        'img': '67.jpg',
+        'position': 66,
+        'name': 'Position no 67'
+    },
+    {
+        'fen': 'nbbnrkqr/pppppppp/8/8/8/8/PPPPPPPP/NBBNRKQR',
+        'img': '68.jpg',
+        'position': 67,
+        'name': 'Position no 68'
+    },
+    {
+        'fen': 'nnbbrkqr/pppppppp/8/8/8/8/PPPPPPPP/NNBBRKQR',
+        'img': '69.jpg',
+        'position': 68,
+        'name': 'Position no 69'
+    },
+    {
+        'fen': 'nnbrkbqr/pppppppp/8/8/8/8/PPPPPPPP/NNBRKBQR',
+        'img': '70.jpg',
+        'position': 69,
+        'name': 'Position no 70'
+    },
+    {
+        'fen': 'nnbrkqrb/pppppppp/8/8/8/8/PPPPPPPP/NNBRKQRB',
+        'img': '71.jpg',
+        'position': 70,
+        'name': 'Position no 71'
+    },
+    {
+        'fen': 'nbnrbkqr/pppppppp/8/8/8/8/PPPPPPPP/NBNRBKQR',
+        'img': '72.jpg',
+        'position': 71,
+        'name': 'Position no 72'
+    },
+    {
+        'fen': 'nnrbbkqr/pppppppp/8/8/8/8/PPPPPPPP/NNRBBKQR',
+        'img': '73.jpg',
+        'position': 72,
+        'name': 'Position no 73'
+    },
+    {
+        'fen': 'nnrkbbqr/pppppppp/8/8/8/8/PPPPPPPP/NNRKBBQR',
+        'img': '74.jpg',
+        'position': 73,
+        'name': 'Position no 74'
+    },
+    {
+        'fen': 'nnrkbqrb/pppppppp/8/8/8/8/PPPPPPPP/NNRKBQRB',
+        'img': '75.jpg',
+        'position': 74,
+        'name': 'Position no 75'
+    },
+    {
+        'fen': 'nbnrkqbr/pppppppp/8/8/8/8/PPPPPPPP/NBNRKQBR',
+        'img': '76.jpg',
+        'position': 75,
+        'name': 'Position no 76'
+    },
+    {
+        'fen': 'nnrbkqbr/pppppppp/8/8/8/8/PPPPPPPP/NNRBKQBR',
+        'img': '77.jpg',
+        'position': 76,
+        'name': 'Position no 77'
+    },
+    {
+        'fen': 'nnrkqbbr/pppppppp/8/8/8/8/PPPPPPPP/NNRKQBBR',
+        'img': '78.jpg',
+        'position': 77,
+        'name': 'Position no 78'
+    },
+    {
+        'fen': 'nnrkqrbb/pppppppp/8/8/8/8/PPPPPPPP/NNRKQRBB',
+        'img': '79.jpg',
+        'position': 78,
+        'name': 'Position no 79'
+    },
+    {
+        'fen': 'bbnnrkrq/pppppppp/8/8/8/8/PPPPPPPP/BBNNRKRQ',
+        'img': '80.jpg',
+        'position': 79,
+        'name': 'Position no 80'
+    },
+    {
+        'fen': 'bnnbrkrq/pppppppp/8/8/8/8/PPPPPPPP/BNNBRKRQ',
+        'img': '81.jpg',
+        'position': 80,
+        'name': 'Position no 81'
+    },
+    {
+        'fen': 'bnnrkbrq/pppppppp/8/8/8/8/PPPPPPPP/BNNRKBRQ',
+        'img': '82.jpg',
+        'position': 81,
+        'name': 'Position no 82'
+    },
+    {
+        'fen': 'bnnrkrqb/pppppppp/8/8/8/8/PPPPPPPP/BNNRKRQB',
+        'img': '83.jpg',
+        'position': 82,
+        'name': 'Position no 83'
+    },
+    {
+        'fen': 'nbbnrkrq/pppppppp/8/8/8/8/PPPPPPPP/NBBNRKRQ',
+        'img': '84.jpg',
+        'position': 83,
+        'name': 'Position no 84'
+    },
+    {
+        'fen': 'nnbbrkrq/pppppppp/8/8/8/8/PPPPPPPP/NNBBRKRQ',
+        'img': '85.jpg',
+        'position': 84,
+        'name': 'Position no 85'
+    },
+    {
+        'fen': 'nnbrkbrq/pppppppp/8/8/8/8/PPPPPPPP/NNBRKBRQ',
+        'img': '86.jpg',
+        'position': 85,
+        'name': 'Position no 86'
+    },
+    {
+        'fen': 'nnbrkrqb/pppppppp/8/8/8/8/PPPPPPPP/NNBRKRQB',
+        'img': '87.jpg',
+        'position': 86,
+        'name': 'Position no 87'
+    },
+    {
+        'fen': 'nbnrbkrq/pppppppp/8/8/8/8/PPPPPPPP/NBNRBKRQ',
+        'img': '88.jpg',
+        'position': 87,
+        'name': 'Position no 88'
+    },
+    {
+        'fen': 'nnrbbkrq/pppppppp/8/8/8/8/PPPPPPPP/NNRBBKRQ',
+        'img': '89.jpg',
+        'position': 88,
+        'name': 'Position no 89'
+    },
+    {
+        'fen': 'nnrkbbrq/pppppppp/8/8/8/8/PPPPPPPP/NNRKBBRQ',
+        'img': '90.jpg',
+        'position': 89,
+        'name': 'Position no 90'
+    },
+    {
+        'fen': 'nnrkbrqb/pppppppp/8/8/8/8/PPPPPPPP/NNRKBRQB',
+        'img': '91.jpg',
+        'position': 90,
+        'name': 'Position no 91'
+    },
+    {
+        'fen': 'nbnrkrbq/pppppppp/8/8/8/8/PPPPPPPP/NBNRKRBQ',
+        'img': '92.jpg',
+        'position': 91,
+        'name': 'Position no 92'
+    },
+    {
+        'fen': 'nnrbkrbq/pppppppp/8/8/8/8/PPPPPPPP/NNRBKRBQ',
+        'img': '93.jpg',
+        'position': 92,
+        'name': 'Position no 93'
+    },
+    {
+        'fen': 'nnrkrbbq/pppppppp/8/8/8/8/PPPPPPPP/NNRKRBBQ',
+        'img': '94.jpg',
+        'position': 93,
+        'name': 'Position no 94'
+    },
+    {
+        'fen': 'nnrkrqbb/pppppppp/8/8/8/8/PPPPPPPP/NNRKRQBB',
+        'img': '95.jpg',
+        'position': 94,
+        'name': 'Position no 95'
+    },
+    {
+        'fen': 'bbqnrnkr/pppppppp/8/8/8/8/PPPPPPPP/BBQNRNKR',
+        'img': '96.jpg',
+        'position': 95,
+        'name': 'Position no 96'
+    },
+    {
+        'fen': 'bqnbrnkr/pppppppp/8/8/8/8/PPPPPPPP/BQNBRNKR',
+        'img': '97.jpg',
+        'position': 96,
+        'name': 'Position no 97'
+    },
+    {
+        'fen': 'bqnrnbkr/pppppppp/8/8/8/8/PPPPPPPP/BQNRNBKR',
+        'img': '98.jpg',
+        'position': 97,
+        'name': 'Position no 98'
+    },
+    {
+        'fen': 'bqnrnkrb/pppppppp/8/8/8/8/PPPPPPPP/BQNRNKRB',
+        'img': '99.jpg',
+        'position': 98,
+        'name': 'Position no 99'
+    },
+    {
+        'fen': 'qbbnrnkr/pppppppp/8/8/8/8/PPPPPPPP/QBBNRNKR',
+        'img': '100.jpg',
+        'position': 99,
+        'name': 'Position no 100'
+    },
+]
